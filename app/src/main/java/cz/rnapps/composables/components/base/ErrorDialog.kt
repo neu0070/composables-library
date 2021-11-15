7bb3efebff581e845e7cb90b2dd4b078b53a05e5
robinneubauer <robinneu@icloud.com>
2021-11-15 17:06:52 +0100

    update

--- app/src/main/java/cz/rnapps/composables/components/base/ErrorDialog.kt
+++ app/src/main/java/cz/rnapps/composables/components/base/ErrorDialog.kt
@@ -9,34 +9,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import cz.rnapps.composables.R
-import cz.rnapps.composables.utils.error.IAppError
 
 @Composable
 fun ErrorDialog(
-    error: IAppError?,
+    errorText: String,
     onDialogDismiss: () -> Unit
 ) {
-    if(error != null) {
-        AlertDialog(
-            onDismissRequest = onDialogDismiss,
-            confirmButton = {
-                Button(onClick = onDialogDismiss) {
-                    Text(text = stringResource(R.string.button_ok))
-                }
-            },
-            title = {
-                Text(text = stringResource(R.string.dialog_title_error))
-            },
-            text = {
-                when {
-                    error.errorMessageResId != null -> {
-                        Text(text = stringResource(id = error.errorMessageResId))
-                    }
-                    error.throwable != null -> {
-                        Text(text = error.throwable.toString())
-                    }
-                }
-            }, modifier = Modifier.padding(horizontal = 8.dp)
-        )
-    }
+    AlertDialog(
+        onDismissRequest = onDialogDismiss,
+        confirmButton = {
+            Button(onClick = onDialogDismiss) {
+                Text(text = stringResource(R.string.button_ok))
+            }
+        },
+        title = {
+            Text(text = stringResource(R.string.dialog_title_error))
+        },
+        text = {
+            Text(text = errorText)
+        }, modifier = Modifier.padding(horizontal = 8.dp)
+    )
 }
--- app/src/main/java/cz/rnapps/composables/components/custom/CustomDatePicker.kt
+++ app/src/main/java/cz/rnapps/composables/components/custom/CustomDatePicker.kt
@@ -4,16 +4,13 @@ import android.app.DatePickerDialog
 import android.content.Context
 import android.widget.DatePicker
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import cz.rnapps.composables.utils.date.DateUtils
 import java.util.*
 
 @Composable
 fun CustomDatePicker(
     context: Context,
     onClosed: () -> Unit,
-    onConfirmed: (String) -> Unit
+    onConfirmed: (Date) -> Unit
 ) {
     val mYear: Int
     val mMonth: Int
@@ -23,20 +20,14 @@ fun CustomDatePicker(
     mMonth = now.get(Calendar.MONTH)
     mDay = now.get(Calendar.DAY_OF_MONTH)
     now.time = Date()
-    val date = remember { mutableStateOf("") }
     val datePickerDialog = DatePickerDialog(
         context,
         { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
             val cal = Calendar.getInstance()
             cal.set(year, month, dayOfMonth)
-            date.value = DateUtils.dateToString(cal.time)
+            onConfirmed(cal.time)
         }, mYear, mMonth, mDay
     )
-    datePickerDialog.setOnDateSetListener { _, year, month, dayOfMonth ->
-        val cal = Calendar.getInstance()
-        cal.set(year, month, dayOfMonth)
-        onConfirmed(DateUtils.dateToString(cal.time))
-    }
     datePickerDialog.setOnDismissListener {
         onClosed()
     }
